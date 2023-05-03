d8f09d373d5a133d9be725e73c82b0340515f3dc
DanielRam06 <122307845+DanielRam06@users.noreply.github.com>
2023-05-03 11:01:17 -0600

    Add files via upload

---
+++ src/Starter.java
@@ -0,0 +1,8 @@
+
+public class Starter { // Define la clase Starter
+
+    public static void main(String[] args) { // Método principal de la clase
+        MyWindow my = new MyWindow(1300,800); // Crea una nueva instancia de MyWindow con ancho 1300 y alto 800
+    }
+
+}
